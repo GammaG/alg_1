cde1b565c5c8f3043f9b8502c77e12e939cb2d2d
GammaG <m_seidler@hotmail.de>
2014-10-25 13:46:34 +0200

    first commit

---
+++ EuclideanAlgorithm/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EuclideanAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.numericUpDown_a = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_b = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Results = new System.Windows.Forms.TextBox();
+            this.button_loops = new System.Windows.Forms.Button();
+            this.numericUpDown_loops = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_Method = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_getGCD = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_b)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_loops)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown_a
+            // 
+            this.numericUpDown_a.Location = new System.Drawing.Point(66, 16);
+            this.numericUpDown_a.Maximum = new decimal(new int[] {
+            1569325056,
+            23283064,
+            0,
+            0});
+            this.numericUpDown_a.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_a.Name = "numericUpDown_a";
+            this.numericUpDown_a.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown_a.TabIndex = 1;
+            this.numericUpDown_a.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_b
+            // 
+            this.numericUpDown_b.Location = new System.Drawing.Point(66, 42);
+            this.numericUpDown_b.Maximum = new decimal(new int[] {
+            1569325056,
+            23283064,
+            0,
+            0});
+            this.numericUpDown_b.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_b.Name = "numericUpDown_b";
+            this.numericUpDown_b.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown_b.TabIndex = 2;
+            this.numericUpDown_b.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b=";
+            // 
+            // textBox_Results
+            // 
+            this.textBox_Results.Location = new System.Drawing.Point(12, 124);
+            this.textBox_Results.Multiline = true;
+            this.textBox_Results.Name = "textBox_Results";
+            this.textBox_Results.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Results.Size = new System.Drawing.Size(338, 256);
+            this.textBox_Results.TabIndex = 6;
+            // 
+            // button_loops
+            // 
+            this.button_loops.Location = new System.Drawing.Point(207, 38);
+            this.button_loops.Name = "button_loops";
+            this.button_loops.Size = new System.Drawing.Size(135, 77);
+            this.button_loops.TabIndex = 7;
+            this.button_loops.Text = "Get GCD over loops";
+            this.button_loops.UseVisualStyleBackColor = true;
+            this.button_loops.Click += new System.EventHandler(this.button_loops_Click);
+            // 
+            // numericUpDown_loops
+            // 
+            this.numericUpDown_loops.Location = new System.Drawing.Point(255, 15);
+            this.numericUpDown_loops.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown_loops.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_loops.Name = "numericUpDown_loops";
+            this.numericUpDown_loops.Size = new System.Drawing.Size(95, 20);
+            this.numericUpDown_loops.TabIndex = 8;
+            this.numericUpDown_loops.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "loops=";
+            // 
+            // comboBox_Method
+            // 
+            this.comboBox_Method.FormattingEnabled = true;
+            this.comboBox_Method.Items.AddRange(new object[] {
+            "Subtraction",
+            "Modulo",
+            "PrimeFactors"});
+            this.comboBox_Method.Location = new System.Drawing.Point(65, 68);
+            this.comboBox_Method.Name = "comboBox_Method";
+            this.comboBox_Method.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Method.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Method:";
+            // 
+            // button_getGCD
+            // 
+            this.button_getGCD.Location = new System.Drawing.Point(66, 95);
+            this.button_getGCD.Name = "button_getGCD";
+            this.button_getGCD.Size = new System.Drawing.Size(120, 23);
+            this.button_getGCD.TabIndex = 12;
+            this.button_getGCD.Text = "Get GCD";
+            this.button_getGCD.UseVisualStyleBackColor = true;
+            this.button_getGCD.Click += new System.EventHandler(this.button_getGCD_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(354, 15);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(472, 366);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+         
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 387);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button_getGCD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox_Method);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown_loops);
+            this.Controls.Add(this.button_loops);
+            this.Controls.Add(this.textBox_Results);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown_b);
+            this.Controls.Add(this.numericUpDown_a);
+            this.Name = "Form1";
+            this.Text = "GCD";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_b)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_loops)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown_a;
+        private System.Windows.Forms.NumericUpDown numericUpDown_b;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Results;
+        private System.Windows.Forms.Button button_loops;
+        private System.Windows.Forms.NumericUpDown numericUpDown_loops;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_Method;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_getGCD;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ EuclideanAlgorithm/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuclideanAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public static int numIterations;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox_Method.SelectedIndex = 0;  //Sub
+        }
+
+        private void button_getGCD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ulong l_a = Convert.ToUInt64(numericUpDown_a.Value);
+                ulong l_b = Convert.ToUInt64(numericUpDown_b.Value);
+
+                Stopwatch timer = new Stopwatch();   //other way to initialize: Stopwatch timer = Stopwatch.StartNew();
+
+                timer.Start();
+
+                ulong gcd;
+
+                numIterations = 0;
+
+                switch (comboBox_Method.SelectedIndex)
+                {
+                    case 0:
+                        gcd = getGCDSub(l_a, l_b);
+                        textBox_Results.AppendText("\r\n Subtraction Method: GCD=" + gcd.ToString());
+                        break;
+                    case 1:
+                        gcd = getGCDMod(l_a, l_b);
+                        textBox_Results.AppendText("\r\n Modulo Method: GCD=" + gcd.ToString());
+                        break;
+                    case 2:
+                        gcd = getGCDPrimeFactorization(l_a, l_b);
+                        textBox_Results.AppendText("\r\n PrimeFactorization Method: GCD=" + gcd.ToString());
+                        break;
+                    default:
+                        return;
+                }
+
+                timer.Stop();
+
+                textBox_Results.AppendText("\r\n CPU-time(ticks):" + timer.ElapsedTicks);
+                textBox_Results.AppendText("\r\n CPU-time(ms):" + timer.ElapsedMilliseconds);
+                textBox_Results.AppendText("\r\n Number of iterations:" + numIterations);
+            }
+            catch (Exception ex)
+            {
+                textBox_Results.AppendText("Your input is wasn't valid!");
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static ulong getGCDMod(ulong a, ulong b)
+        {
+
+            while (b != 0)
+            {
+                numIterations++;
+                ulong t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        public static List<ulong> getPrimeNumbers(ulong n)
+        {
+           
+            List<ulong> primeNumbers = new List<ulong>();
+            for (ulong i = 2; i <= n; i++)
+            {
+                numIterations++;
+                while (n % i == 0 )
+                {
+                    
+                    primeNumbers.Add(i);
+                    n /= i;
+                   
+
+                }
+            }
+
+            return primeNumbers;
+        }
+
+        public static ulong getGCDPrimeFactorization(ulong a, ulong b)
+        {
+            
+            List<ulong> primeNumbersA = getPrimeNumbers(a);
+            List<ulong> primeNumbersB = getPrimeNumbers(b);
+            List<ulong> commonFactors = new List<ulong>();
+            foreach (ulong _a in primeNumbersA){
+                foreach (ulong _b in primeNumbersB)
+                {
+                    numIterations++;
+                    if (_a == _b & !commonFactors.Contains(_a))
+                        commonFactors.Add(_a);
+                }
+            }
+
+            if (!commonFactors.Any())
+            {
+                return 0;
+            }
+            ulong x = 1;
+            foreach (ulong factor in commonFactors)
+            {
+                numIterations++;
+                x *= factor;
+            }
+            return x;
+            
+        }
+
+        public static ulong getGCDSub(ulong a, ulong b)
+        {
+            if (a == 0)
+                return b;
+
+            while (b != 0)
+            {
+                numIterations++;
+
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+
+            return a;
+        }
+
+
+        /** LOOPS */
+        private void button_loops_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ulong l_a = Convert.ToUInt64(numericUpDown_a.Value);
+                ulong l_b = Convert.ToUInt64(numericUpDown_b.Value);
+
+                List<long> listCPUTimes = new List<long>();
+
+                int numOfLoops = (int)numericUpDown_loops.Value;
+
+                Stopwatch timer = new Stopwatch();   //other way to initialize: Stopwatch timer = Stopwatch.StartNew();
+
+                for (int i = 0; i < numOfLoops; i++)
+                {
+                    timer.Reset();
+                    timer.Start();
+
+                    switch (comboBox_Method.SelectedIndex)
+                    {
+                        case 0:
+                            getGCDSub(l_a, l_b);
+                            break;
+                        case 1:
+                            getGCDMod(l_a, l_b);
+                            break;
+                        default:
+                            return;
+                    }
+
+                    timer.Stop();
+                    listCPUTimes.Add(timer.ElapsedTicks);
+                    textBox_Results.AppendText("\r\n Iteration " + i.ToString() + ", CPU-time(ticks):" + timer.ElapsedTicks);
+                }
+
+                //Get Mean and SD
+                double meanCPUTicks = getMean(listCPUTimes);
+                double varianceCPUTicks = getVariance(listCPUTimes);
+                double standardDeviationCPUTicks = Math.Sqrt(varianceCPUTicks);
+
+                textBox_Results.AppendText("\r\n Mean CPU-time(ticks):" + meanCPUTicks);
+                textBox_Results.AppendText("\r\n Standard Deviation CPU-time(ticks):" + standardDeviationCPUTicks);
+
+                //Get Median
+                //ToDo: your implementation
+
+                //Get Histogram            
+                //ToDo: your implementation
+                long startHisto = 0; //get min value
+                long endHisto = 0; //get max value
+
+                List<int> histo = getHistogram(startHisto, endHisto, listCPUTimes);
+
+                //Get Mode
+                //ToDo: your implementation
+
+
+
+                //show normalized histogram, probability density of CPU-time (ticks)
+                double[] histoNormalized = getNormalizedHistogram(startHisto, endHisto, listCPUTimes);
+                textBox_Results.AppendText("\r\n Normalized histogram:");
+                for (int i = 0; i < histoNormalized.Count(); i++)
+                    textBox_Results.AppendText("\r\n" + i.ToString() + ": " + histoNormalized[i]);
+
+                //add data to chart
+                chart1.Series[0].Points.Clear();
+
+                //ToDo: your implementation
+                double cpuTicksHistoCounter = 0;
+
+                foreach (double probCPUTicks in histoNormalized)
+                {
+                    //add datapoint X,Y to chart
+                    chart1.Series[0].Points.AddXY(cpuTicksHistoCounter, probCPUTicks);
+
+                    //compute next counter
+                    //ToDo: your implementation
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox_Results.AppendText("Your input is wasn't valid!");
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        public static double getMean(List<long> resultset)
+        {
+            //ToDo: your implementation
+			return 0;
+        }
+
+        public static double getVariance(List<long> resultSet)
+        {
+            //ToDo: your implementation
+			return 0;
+        }
+
+        public static List<int> getHistogram(double start, double end, List<long> data)
+        {
+			//ToDo: your implementation
+            int num_bins = 1;
+
+            List<int> histo = new List<int>(num_bins);
+
+            return histo;
+        }
+
+        public static List<int> genRandomList(int min, int max, int count)
+        {
+            List<int> randomList = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < count; ++i)
+            {
+                randomList.Add(random.Next(min, max + 1));
+            }
+            return randomList;
+        }
+
+        public static double[] getNormalizedHistogram(double start, double end, List<long> data)
+        {
+			//ToDo: your implementation
+            int num_bins = (int)Math.Round(Math.Sqrt(data.Count));
+
+            double[] histo = new double[num_bins];
+
+            
+            return histo;
+        }
+
+       
+
+    }
+}
